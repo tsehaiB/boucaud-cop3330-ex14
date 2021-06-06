3a57d9ede778a4f2b1d79e7eb7236992c1d40a3b
Art3mis <tsehaib@knights.ucf.edu>
2021-06-06 14:41:14 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,32 @@
+package base;
+import java.util.Scanner;
+public class App {
+    final static double taxRate = .055;
+    Scanner inp = new Scanner(System.in);
+    public static void main(String[] args){
+        App myApp = new App();
+        int amount = myApp.readAmount();
+        String state = myApp.readState();
+        double total = amount;
+        if(state.equals("WI")){
+            double tax = myApp.calcTax(amount);
+            System.out.println("The tax is $" + tax + ".");
+            total = myApp.calcTotal(amount, tax);
+        }
+        System.out.println("The total is $" + total + ".");
+    }
+    private int readAmount(){
+        System.out.print("What is the order amount? ");
+        return Integer.parseInt(inp.nextLine());
+    }
+    private String readState(){
+        System.out.print("What is the state? ");
+        return inp.nextLine();
+    }
+    private double calcTax(int amount){
+        return amount * taxRate;
+    }
+    private double calcTotal(int amount, double tax){
+        return amount + tax;
+    }
+}
